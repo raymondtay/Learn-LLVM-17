e44b40d9cf1c91cd4729cd6b505e5ae46d449e64
Raymond Tay <raymondtay1974@gmail.com>
2025-08-09 15:30:05 +0800

    factor: cleaned the build script

--- Chapter04/tinylang/build_n_run.sh
+++ Chapter04/tinylang/build_n_run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir build
-cd build
-cmake ../ && cmake --build .
+BUILD_DIR=build
+cmake -S . -B $BUILD_DIR && cmake --build $BUILD_DIR
+
 echo '1. Compiler built'
-./tools/driver/tinylang --filetype=obj ../examples/Gcd.mod
+./$BUILD_DIR/tools/driver/tinylang --filetype=obj ./examples/Gcd.mod
 echo '2. Compiler translated and built "Gcd.mod"'
 echo '3. Compiler integrated with Gcd.o object file'
-clang ../examples/callgcd.c ../examples/Gcd.o -o gcd
+clang ./examples/callgcd.c ./examples/Gcd.o -o gcd
 echo '4. Running final program'
 ./gcd
